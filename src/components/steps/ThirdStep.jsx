594b2c7ee07731080f57deea2967360323c7a50d
Jay Mingle <jkawudi@gmail.com>
2024-02-06 12:45:22 +0000

    Changes

--- src/components/steps/ThirdStep.jsx
+++ src/components/steps/ThirdStep.jsx
@@ -5,9 +5,9 @@ const ThirdStep = () => {
     return (
         <div className="step_container">
             <h3>Step 3</h3>
-            <TextField label="First Name" margin="normal" variant="outlined" color="secondary"/>
-            <TextField label="Last Name" margin="normal" variant="outlined" color="secondary"/>
-            <TextField label="Contact Number" margin="normal" variant="outlined" color="secondary"/>
+            <TextField label="City" margin="normal" variant="outlined" color="secondary"/>
+            <TextField label="Landmark" margin="normal" variant="outlined" color="secondary"/>
+            <TextField label="Postal Address" margin="normal" variant="outlined" color="secondary"/>
             <div className="buttons">
                 <Button className="button" variant="contained" color="primary">Next</Button>
             </div>
