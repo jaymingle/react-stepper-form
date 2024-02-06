820896aecb1f75c9f399e7af384c7d200188c8b8
Jay Mingle <jkawudi@gmail.com>
2024-02-06 13:21:39 +0000

    Changes

---
+++ src/components/StepContext.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+const StepContext= () => {
+
+    const [surrentStep, setCurrentStep] = useState(1)
+    const [userData, setUserData] = useState([])
+    const [finalData, setFinalData] = useState([])
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default StepContext;
