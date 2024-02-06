a5cb7b190ed8de513b27c80d33034634238500ea
Jay Mingle <jkawudi@gmail.com>
2024-02-06 03:02:16 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import UserForm from "./components/UserForm.jsx";
 import FirstStep from "./components/steps/FirstStep.jsx";
 
 function App() {
--- src/components/steps/FirstStep.jsx
+++ src/components/steps/FirstStep.jsx
@@ -4,7 +4,9 @@ import {Button, TextField} from '@mui/material'
 const FirstStep = () => {
     return (
         <div>
-            
+            <h3>Step 1</h3>
+                <TextField label="First Name" margin="normal" variant={"outlined"} color={"secondary"}/>
+               {/*<Button>Welcome</Button>*/}
         </div>
     );
 };
