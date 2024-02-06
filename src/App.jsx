0cef5b4bfff51da07dd83b359a618c2bf0e5cf07
Jay Mingle <jkawudi@gmail.com>
2024-02-06 03:26:20 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import React, {useState} from 'react'
+
 import './App.css'
 import FirstStep from "./components/steps/FirstStep.jsx";
+import SecondStep from "./components/steps/SecondStep.jsx";
+import ThirdStep from "./components/steps/ThirdStep.jsx";
+import DisplayData from "./components/DisplayData.jsx";
+import {Stepper, StepLabel, Step} from "@mui/material";
 
 function App() {
 
+  const [step, setStep] = useState(1)
+
   return (
     <div className="app_container">
+        <h3 style={{color: "red", fontSize: "2rem", textDecoration: "underline"}}>Multi Step Form</h3>
           <FirstStep/>
+          <SecondStep/>
+          <ThirdStep/>
+          <DisplayData/>
+
+      <div className="center-stepper">
+          <Stepper style={{width: '18%'}} activeStep={"1"} orientation="horizontal">
+              <Step>
+                  <StepLabel></StepLabel>
+              </Step>
+            <Step>
+                  <StepLabel></StepLabel>
+              </Step>
+            <Step>
+                  <StepLabel></StepLabel>
+              </Step>
+          </Stepper>
+      </div>
+
     </div>
   )
 }
--- src/components/steps/SecondStep.jsx
+++ src/components/steps/SecondStep.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Button, TextField} from "@mui/material";
+import "../../App.css"
 
 const SecondStep = () => {
     return (
-        <div>
-
+        <div className="step_container">
+            <h3>Step 2</h3>
+            <TextField label="First Name" margin="normal" variant="outlined" color="secondary"/>
+            <TextField label="Last Name" margin="normal" variant="outlined" color="secondary"/>
+            <TextField label="Contact Number" margin="normal" variant="outlined" color="secondary"/>
+            <Button variant="contained" color="primary">Next</Button>
         </div>
     );
 };
